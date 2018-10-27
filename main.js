548c9014ce440f1fb9b5ee4e78d248ca6e753213
bgudini <protasov.glebos@yandex.ru>
2018-10-27 12:19:28 +0300

    qq

---
+++ main.js
@@ -0,0 +1,8 @@
+$('.driver__img').click(function(){
+    $('.driver__inform').toggleClass('driver__inform_active');
+    $('.driver__img').toggleClass('driver__img_active');
+});
+$('.driver__img2').click(function(){
+    $('.driver__inform2').toggleClass('driver__inform_active2');
+    $('.driver__img2').toggleClass('driver__img_active');
+});
